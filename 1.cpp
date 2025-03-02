9b351b4cf44c711d183119da4759757e06dbf54c
rilangzheng <3084786581@qq.com>
2025-03-02 20:07:28 +0800

    aaa

---
+++ 1.cpp
@@ -0,0 +1,36 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+enum sex
+{
+	b,
+	c,
+	d,
+	e=3,
+
+};
+//枚举类型，定义出sex作为全集
+	
+int main(void)
+{
+
+	int a;
+	int b;
+	printf("第一个\n");	
+	scanf("%d", &a);
+	printf("第二个\n");
+	 
+	scanf("%d", &b);
+	
+	
+
+		int sum = a+b;
+		printf(a+b);
+	return 0;
+}
+//int main(void) {
+//	int i;
+//	for i = range(5);
+//
+//	return 0;
+//}
